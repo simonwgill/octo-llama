c1ef3c45784d74b4e5bd7129f0b2a96474ee831f
Simon W. Gill <simon@patternwebsolutions.com>
2014-06-13 22:29:40 +0100

    Added the basic interface for all apps running on the platform, the Llama.

---
+++ llama/llama.py
@@ -0,0 +1,36 @@
+import pickle
+
+DEFAULT_SLEEP_TIME = 0.5
+
+class Llama(object):
+  def __init__(self, client, queuename):
+    self.client = client
+    self.queuename = queuename
+
+  def handle_pika_delivery(self, channel, method, header, body):
+    self.handle(channel, method.delivery_tag, body)
+
+  def handle(self, channel, delivery_tag, body):
+    message = pickle.loads(body)
+    channel.basic_ack(delivery_tag = delivery_tag)
+    self.do_message(message)
+
+  def monitor(self):
+    self.client.monitor(self.qname, self.handle_pika_delivery)
+
+  def publish(self, message):
+    self.client.publish(pickle.dumps(message), routing_key="")
+
+  def get_sleep_time(self):
+    return DEFAULT_SLEEP_TIME
+
+  def begin_wait_loop(self):
+    while True:
+      self.do_action()
+      time.sleep(self.get_sleep_time())
+      
+  def do_action(self):
+    pass
+
+  def d0_message(self, message):
+    pass
