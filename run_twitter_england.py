f85c068871ca894f63915bc3e42bc8a9cda166f4
Simon W. Gill <simon@patternwebsolutions.com>
2014-06-21 13:29:31 +0100

    Added debug log level to listener.

--- run_twitter_england.py
+++ run_twitter_england.py
@@ -1,7 +1,9 @@
 import os
-
+import logging
 parentdir = os.path.dirname(os.path.abspath(__file__))
 os.sys.path.insert(0,parentdir)
+
+logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.DEBUG)
 
 from llama.pika_client import *
 publisher = PikaPublisher("demonstration")
--- run_twitter_france.py
+++ run_twitter_france.py
@@ -1,7 +1,10 @@
 import os
-
+import logging
 parentdir = os.path.dirname(os.path.abspath(__file__))
 os.sys.path.insert(0,parentdir)
+
+logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.DEBUG)
+
 
 from llama.pika_client import *
 publisher = PikaPublisher("demonstration")
