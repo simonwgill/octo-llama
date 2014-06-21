b8df829539f4e11ece84ae16e6b3370c65dd78ca
Simon W. Gill <simon@patternwebsolutions.com>
2014-06-21 10:59:19 +0100

    Added logging. Added check for correct queue when handling message. Added rejects when messages are badly formed.

--- llama/llama.py
+++ llama/llama.py
@@ -2,6 +2,8 @@ import time
 import pickle
 import threading
 import base64
+import traceback
+import logging
 
 DEFAULT_SLEEP_TIME = 0.5
 
@@ -14,18 +16,26 @@ class Llama(object):
     self.handle(channel, method.delivery_tag, body)
 
   def handle(self, channel, delivery_tag, body):
-    message = pickle.loads(body)
-    channel.basic_ack(delivery_tag = delivery_tag)
-    self.do_message(message)
+    try:
+      message = pickle.loads(body)
+      if (message[0] != self.queuename):
+        return
+      logging.debug("Ack of delivery_tag %s" % delivery_tag)
+      channel.basic_ack(delivery_tag = delivery_tag)
+      self.do_message(message[1])
+    except:
+      logging.warning("Rejecting message with delivery_tag %s due to exception:\n %s" % (delivery_tag, traceback.format_exc()))
+      channel.basic_reject(delivery_tag = delivery_tag, requeue=False)
 
   def monitor(self):
     self.monitorthread = threading.Thread(target=self.client.monitor, args=(self.queuename, self.handle_pika_delivery))
+    self.monitorthread.daemon = True
     self.monitorthread.start()
 
   def publish(self, message, queuename=None):
     if queuename is None:
       queuename = self.queuename
-    body = pickle.dumps(message, 2)
+    body = pickle.dumps((queuename, message), 2)
     self.client.publish(body, routing_key=queuename)
 
   def get_sleep_time(self):
